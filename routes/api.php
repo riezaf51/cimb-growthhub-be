3619eb76b1abe67b47626a8cb2794c60851c438c
riezaf51 <riezaf51@gmail.com>
2025-02-13 14:41:15 +0700

    style: change api training response style

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\TrainingController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
@@ -17,11 +16,16 @@ Route::get('/', function () {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [TraineeController::class, 'register']);
 
+Route::apiResource('/trainings', TrainingController::class)->only(['index', 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/logout', [AuthController::class, 'logout']);
     Route::get('/user-by-token', [AuthController::class, 'me']);
 
     Route::get('/roles', [RoleController::class, 'index']);
+
+    Route::post('/trainings/enroll', [TrainingController::class, 'enroll']);
+    Route::post('/trainings/cancel-enrollment', [TrainingController::class, 'cancelEnrollment']);
 
     // CMS Only Routes
     Route::middleware(RoleMiddleware::class . ':admin|hr')->group(function () {
@@ -30,15 +34,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/users/{id}', [UserController::class, 'show']);
         Route::put('/users/{id}', [UserController::class, 'update']);
         Route::delete('/users/{id}', [UserController::class, 'destroy'])->middleware(RoleMiddleware::class . ':admin');
+
+        Route::apiResource('/trainings', TrainingController::class)->only(['store', 'update', 'destroy']);
     });
 });
 
-Route::apiResource('/trainings', TrainingController::class);
-
-Route::post('/trainings/enroll', [TrainingController::class, 'enroll'])->middleware('auth:sanctum');
-
-Route::post('/trainings/cancel-enrollment', [TrainingController::class, 'cancelEnrollment'])->middleware('auth:sanctum');
-
 Route::fallback(function () {
-    return ApiFormatter::createApi(false, 'Page not found', null, 404);
+    return ApiFormatter::createApi(false, 'URL not found', null, 404);
 });
